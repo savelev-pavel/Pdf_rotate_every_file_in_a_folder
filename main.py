fe4efbfd54148f411eda5032b6b85f28da64405c
pavel.savelev <pal.igory4@gmail.com>
2022-09-30 14:49:33 +0300

    update 1

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+"""Создает в указанной папке папку с pdf-файлами,
+ повернутыми на определенное количество градусов"""
+
 import os
 from PyPDF4 import PdfFileReader, PdfFileWriter
 
-def get_file_list(files_path:str):
+def get_file_list(files_path:str) -> list:
     filelist = []
-    for root, dirs, files in os.walk(files_path):
-        for filename in files:
-            filelist.append(filename)
+    for element in os.listdir(files_path):
+        if element.endswith('.pdf'): filelist.append(element)
     return filelist
 
 def main(inputpath:str, filelist:list, turndegrees:int):
@@ -29,9 +31,8 @@ def main(inputpath:str, filelist:list, turndegrees:int):
 
 
 if __name__ == "__main__":
-    # filepath = "R:\\Лаборатория ЭТЛ\\СДС ИНТЕРГАЗСЕРТ\\! ИК №1\ООО Томсккабель\\20.1103.12141.027-ТУ 041-монтажные\\" \
-    #            "9.1.5.5 Доп. материалы\\9. Прием. контроль и период. исп\\Протоколы ПИ\\2021\\"
     filepath = input('Введите путь:\n')
     if not filepath.endswith('\\'): filepath += '\\'
     turndegrees = int(input('Введите угол поворота по часовой стрелке:\n'))
+    # print(get_file_list(filepath))
     main(filepath,get_file_list(filepath),turndegrees)
